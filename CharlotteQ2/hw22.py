e62a80fc817bcd0dd193585fd9b2fb0157ce4939
charlloyd <char.lloyd@gmail.com>
2017-03-06 21:41:25 -0500

    Update hw22.py

--- CharlotteQ2/hw22.py
+++ CharlotteQ2/hw22.py
@@ -14,24 +14,16 @@ import pycuda.autoinit
 kernel_code_template = """
     __global__ void MatrixMulKernel(float *a, float *b, float *c)
     {
-    // 2D Thread ID (assuming that only *one* block will be executed)
     int tx = threadIdx.x;
     int ty = threadIdx.y;
-    
-    // Pvalue is used to store the element of the matrix
-    // that is computed by the thread
     float Pvalue = 0;
     
-    // Each thread loads one row of M and one column of N,
-    //   to produce one element of P.
     for (int k = 0; k < %(MATRIX_SIZE)s; ++k) {
     float Aelement = a[ty * %(MATRIX_SIZE)s + k];
     float Belement = b[k * %(MATRIX_SIZE)s + tx];
     Pvalue += Aelement * Belement;
     }
     
-    // Write the matrix to device memory;
-    // each thread writes one element
     c[ty * %(MATRIX_SIZE)s + tx] = Pvalue;
     }
     """
