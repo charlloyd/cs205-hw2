244d3c9b216be4f6bee02759b7dd8f1ccea178f8
charlloyd <char.lloyd@gmail.com>
2017-03-07 14:09:01 -0500

    Update hw22.py

--- CharlotteQ2/hw22.py
+++ CharlotteQ2/hw22.py
@@ -3,6 +3,7 @@
 # copied from: https://andreask.cs.illinois.edu/PyCuda/Examples/MatrixmulSimple
 
 import numpy as np
+from __future__ import division
 from pycuda import driver, compiler, gpuarray, tools
 import pycuda.autoinit
 
@@ -21,6 +22,63 @@ kernel_code_template = """
     
     c[ty * %(MATRIX_SIZE)s + tx] = Pvalue;
     }
+    
+    __global__ void MatMulBlockKernel(float *A, float *B, float *C)
+    {
+    
+    const uint wA = %(MATRIX_SIZE)s;
+    const uint wB = %(MATRIX_SIZE)s;
+    
+    // Block index
+    const uint bx = blockIdx.x;
+    const uint by = blockIdx.y;
+    
+    // Thread index
+    const uint tx = threadIdx.x;
+    const uint ty = threadIdx.y;
+    
+    // Index of the first sub-matrix of A processed by the block
+    const uint aBegin = wA * %(BLOCK_SIZE)s * by;
+    // Index of the last sub-matrix of A processed by the block
+    const uint aEnd = aBegin + wA - 1;
+    // Step size used to iterate through the sub-matrices of A
+    const uint aStep = %(BLOCK_SIZE)s;
+    
+    // Index of the first sub-matrix of B processed by the block
+    const uint bBegin = %(BLOCK_SIZE)s * bx;
+    // Step size used to iterate through the sub-matrices of B
+    const uint bStep = %(BLOCK_SIZE)s * wB;
+    
+    // The element of the block sub-matrix that is computed by the thread
+    float Csub = 0;
+    // Loop over all the sub-matrices of A and B required to compute the block sub-matrix
+    for (int a = aBegin, b = bBegin;
+    a <= aEnd;
+    a += aStep, b += bStep)
+    {
+    // Shared memory for the sub-matrix of A
+    __shared__ float As[%(BLOCK_SIZE)s][%(BLOCK_SIZE)s];
+    // Shared memory for the sub-matrix of B
+    __shared__ float Bs[%(BLOCK_SIZE)s][%(BLOCK_SIZE)s];
+    
+    // Load the matrices from global memory to shared memory each thread loads one element of each matrix
+    As[ty][tx] = A[a + wA * ty + tx];
+    Bs[ty][tx] = B[b + wB * ty + tx];
+    // Synchronize to make sure the matrices are loaded
+    __syncthreads();
+    
+    // Multiply the two matrices together; each thread computes one element of the block sub-matrix
+    for (int k = 0; k < %(BLOCK_SIZE)s; ++k)
+    Csub += As[ty][k] * Bs[k][tx];
+    
+    // Synchronize to make sure that the preceding computation is done before loading two new sub-matrices of A and B in the next iteration
+    __syncthreads();
+    }
+    
+    // Write the block sub-matrix to global memory; each thread writes one element
+    const uint c = wB * %(BLOCK_SIZE)s * by + %(BLOCK_SIZE)s * bx;
+    C[c + wB * ty + tx] = Csub;
+    }        
     """
 
 MATRIX_SIZES = [8,16,32,64]
@@ -43,7 +101,9 @@ for MATRIX_SIZE in MATRIX_SIZES:
     }
     mod = compiler.SourceModule(kernel_code)
     matmuls = mod.get_function("MatMulSimpleKernel")
-    matmuls(a_gpu, b_gpu, c_gpu, block = (MATRIX_SIZE, MATRIX_SIZE, 1))
+    bdim = (MATRIX_SIZE, MATRIX_SIZE, 1)
+    gdim = ()
+    matmuls(a_gpu, b_gpu, c_gpu, block=bdim, grid=gdim)
 
     print("Matrix A (GPU):")
     #print(a_gpu.get())
