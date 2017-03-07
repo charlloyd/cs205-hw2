1f44ef1d1c04236d977de1a129af12d05eaa7c64
charlloyd <char.lloyd@gmail.com>
2017-03-06 20:07:11 -0500

    Asdf

--- CharlotteQ2/hw22.cu
+++ CharlotteQ2/hw22.cu
@@ -1,36 +1,26 @@
-""" Multiplies two square matrices together using a *single* block of threads and global memory only. Each thread computes one element of the resulting matrix. """
-
 import numpy as np
-from pycuda import driver, compiler, gpuarray, tools
-
-// -- initialize the device
+import pycuda.driver as cuda
 import pycuda.autoinit
+from pycuda.compiler import SourceModule
 
-kernel_code_template = """
-__global__ void MatrixMulKernel(float *a, float *b, float *c)
-{
-// 2D Thread ID (assuming that only *one* block will be executed)
-int tx = threadIdx.x;
-int ty = threadIdx.y;
+mod = SourceModule("""
+    __global__ void MatrixMulKernel(float *a, float *b, float *c)
+    {
+        int tx = threadIdx.x;
+        int ty = threadIdx.y;
+        float Pvalue = 0;
 
-// Pvalue is used to store the element of the matrix that is computed by the thread
-float Pvalue = 0;
+        // Each thread loads one row of M and one column of N, to produce one element of P.
+        for (int k = 0; k < %(MATRIX_SIZE)s; ++k)
+        {
+            float Aelement = a[ty * %(MATRIX_SIZE)s + k];
+            float Belement = b[k * %(MATRIX_SIZE)s + tx];
+            Pvalue += Aelement * Belement;
+        }
 
-// Each thread loads one row of M and one column of N, to produce one element of P.
-for (int k = 0; k < %(MATRIX_SIZE)s; ++k) {
-float Aelement = a[ty * %(MATRIX_SIZE)s + k];
-float Belement = b[k * %(MATRIX_SIZE)s + tx];
-Pvalue += Aelement * Belement;
-}
-
-// Write the matrix to device memory; each thread writes one element
-c[ty * %(MATRIX_SIZE)s + tx] = Pvalue;
-}
-"""
-
-// define the (square) matrix size, note that we'll only use *one* block of threads here
-//  as a consequence this number (squared) can't exceed max_threads,
-//  see http://documen.tician.de/pycuda/util.html#pycuda.tools.DeviceData
+        c[ty * %(MATRIX_SIZE)s + tx] = Pvalue;
+    }
+    """)
 
 MATRIX_SIZE = 2
 
@@ -50,25 +40,23 @@ c_gpu = gpuarray.empty((MATRIX_SIZE, MATRIX_SIZE), np.float32)
 
 // get the kernel code from the template
 // by specifying the constant MATRIX_SIZE
-kernel_code = kernel_code_template % {
-'MATRIX_SIZE': MATRIX_SIZE
+kernel_code = kernel_code_template %
+{
+    'MATRIX_SIZE': MATRIX_SIZE
 }
-
-// compile the kernel code
-mod = compiler.SourceModule(kernel_code)
 
 // get the kernel function from the compiled module
 matrixmul = mod.get_function("MatrixMulKernel")
 
 // call the kernel on the card
 matrixmul(
-// inputs
-a_gpu, b_gpu,
-// output
-c_gpu,
-// (only one) block of MATRIX_SIZE x MATRIX_SIZE threads
-block = (MATRIX_SIZE, MATRIX_SIZE, 1),
-)
+    // inputs
+    a_gpu, b_gpu,
+    // output
+    c_gpu,
+    // (only one) block of MATRIX_SIZE x MATRIX_SIZE threads
+    block = (MATRIX_SIZE, MATRIX_SIZE, 1),
+    )
 
 // print the results
 print "-" * 80
