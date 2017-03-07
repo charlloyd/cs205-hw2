b03fc000daed820272a3a69764e34dbc8d65647c
charlloyd <char.lloyd@gmail.com>
2017-03-06 21:16:41 -0500

    Update hw22.py

--- CharlotteQ2/hw22.py
+++ CharlotteQ2/hw22.py
@@ -11,7 +11,8 @@ mod = SourceModule("""
     {
         int tx = threadIdx.x;
         int ty = threadIdx.y;
-        c[ty * %(MATRIX_SIZE)s + tx] = tx;
+        float Pvalue = 0;
+        c[ty * %(MATRIX_SIZE)s + tx] = Pvalue;
     }
     
     __global__ void getGlobalIdx_1D_1D(int *in_data) 
