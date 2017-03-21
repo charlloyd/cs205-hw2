9d1a4107d26993da7cb19e792f37012c3c4b56a8
charlloyd <char.lloyd@gmail.com>
2017-03-21 15:56:12 -0400

    Update helloacc.cpp

--- helloacc.cpp
+++ helloacc.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main () {
    // for loop execution
-#pragma acc loop
+#pragma omp parallel for shared(a)
    for( int a = 0; a < 16; a = a + 1 ) {
       cout << "Hello World! number: " << a << endl;
    }
