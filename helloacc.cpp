91205d139461e06bde7b47c8b29b3212d6c1e523
charlloyd <char.lloyd@gmail.com>
2017-03-21 15:53:19 -0400

    Update helloacc.cpp

--- helloacc.cpp
+++ helloacc.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 
 // compile with "g++ helloacc.cpp -o helloacc"
-// execute with "pgcc -acc helloacc.cpp -Minfo=accel"
+// compile with "pgc++ -acc helloacc.cpp -Minfo=accel"
+// execute with "./helloacc"
 
 using namespace std;
 
 int main () {
    // for loop execution
+#pragma acc loop
    for( int a = 0; a < 16; a = a + 1 ) {
       cout << "Hello World! number: " << a << endl;
    }
