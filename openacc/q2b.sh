2398a5a718470167a21870137055110e7a2e744d
charlloyd <char.lloyd@gmail.com>
2017-03-21 21:58:09 -0400

    Create q2b.sh

---
+++ openacc/q2b.sh
@@ -0,0 +1,10 @@
+export PGI=/n/seasfs03/IACS/cs205/pgi
+export PATH=$PGI/linux86-64/16.10/bin:$PATH
+export MANPATH=$MANPATH:$PGI/linux86-64/16.10/man
+export LM_LICENSE_FILE=$LM_LICENSE_FILE:$PGI/license.dat
+
+git pull
+g++ q2_b.cpp -o q2_b_seq
+pgc++ -acc q2_b.cpp -Minfo=accel -o q2_b_para
+./q2_b_seq
+./q2_b_para
