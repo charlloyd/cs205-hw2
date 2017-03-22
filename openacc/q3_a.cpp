fbc9fe5a3e5c272937bdf4e3a9ffe3fff4980f40
eifer4 <edunipace@g.harvard.edu>
2017-03-21 21:46:26 -0400

    Update q3_a.cpp

--- openacc/q3_a.cpp
+++ openacc/q3_a.cpp
@@ -2,21 +2,19 @@
 
 void bfs(int *Adj, int *x, int n, int root)
 {
-   #pragma acc enter data copyin(Adj[0:n * n],x[0:n])
+   #pragma acc enter data copyin(Adj[0:n * n], x[0:n])
 
    int *last_x = (int *)malloc(sizeof(int) * n);
 
-   for(int i=0;i < n;i++)
+   for(int i=0;i < n; i++)
    {
-     x[i]=0; last_x[i]=0;
+     x[i] = 0; last_x[i] = 0;
    }
-   x[root]=1; last_x[root]=1;
-
-   bool converged = false;
+   x[root] = 1; last_x[root] = 1;
 
    while(true)
    {
-     copy_from_source_to_destination(x,last_x,n);
+     copy_from_source_to_destination(x, last_x, n);
 
 
      for(int j=0; j < n; j++)
