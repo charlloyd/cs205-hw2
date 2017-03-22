843d286e2429a0913a5e83ca2dc6628419b261b9
charlloyd <char.lloyd@gmail.com>
2017-03-21 20:03:08 -0400

    Update helloacc.cpp

--- helloacc.cpp
+++ helloacc.cpp
@@ -9,7 +9,6 @@ using namespace std;
 int main () {
    // for loop execution
 
-#pragma acc data create(a)
 #pragma acc kernels
    for( int a = 0; a < 16; a = a + 1 ) {
       cout << "Hello World! number: " << a << endl;
