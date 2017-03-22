ffd3fea6343080cdf0ad9204ee2d117d662ae8c5
charlloyd <char.lloyd@gmail.com>
2017-03-21 20:15:11 -0400

    Update helloacc.cpp

--- helloacc.cpp
+++ helloacc.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdlib>
 
 // compile with "g++ helloacc.cpp -o helloacc"
 // compile with "pgc++ -acc helloacc.cpp -Minfo=accel"
@@ -13,7 +14,8 @@ int b[8] = {1,2,3,4,5,6,7,8};
    
 #pragma acc kernels
    for( int a = 0; a < 8; a = a + 1 ) {
-      cout << "Hello World! number: " << b[a] << endl;
+      //cout << "Hello World! number: " << b[a] << endl;
+      printf("Hello World! number: ",b[a]);
    }
  
    return 0;
