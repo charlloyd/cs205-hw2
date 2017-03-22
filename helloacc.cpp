a29cd7fc3ba1e668c8ecf304e16ce21cf6ba32a8
charlloyd <char.lloyd@gmail.com>
2017-03-21 20:37:18 -0400

    Update helloacc.cpp

--- helloacc.cpp
+++ helloacc.cpp
@@ -13,11 +13,10 @@ int main () {
 
 int b[8] = {1,2,3,4,5,6,7,8};
 int total=0;
-int a = 0;
 
 #pragma acc kernels
-#pragma acc data copyin(b), copyout(total), create(a)
-   for( a = 0; a < 8; a = a + 1 ) {
+#pragma acc data copyin(b), copyout(total)
+   for(int a = 0; a < 8; a = a + 1 ) {
       //cout << "Hello World! number: " << b[a] << endl;
       total += b[a];
    }
