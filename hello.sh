94298ed424ed4b39c7edcd084adf3160ee015ab5
charlloyd <char.lloyd@gmail.com>
2017-03-21 20:23:06 -0400

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -4,6 +4,6 @@ export MANPATH=$MANPATH:$PGI/linux86-64/16.10/man
 export LM_LICENSE_FILE=$LM_LICENSE_FILE:$PGI/license.dat
 
 git pull
-g++ helloacc.cpp -o helloacc
-pgc++ -acc helloacc.cpp -Minfo=accel
+g++ helloacc.cpp -o helloacc_seq
+pgc++ -acc helloacc.cpp -Minfo=accel -o helloacc_para
 ./helloacc
