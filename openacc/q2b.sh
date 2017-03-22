949f56716bef6071689261e909c20c0426b8a692
charlloyd <char.lloyd@gmail.com>
2017-03-21 22:11:45 -0400

    Update q2b.sh

--- openacc/q2b.sh
+++ openacc/q2b.sh
@@ -3,7 +3,7 @@ export PATH=$PGI/linux86-64/16.10/bin:$PATH
 export MANPATH=$MANPATH:$PGI/linux86-64/16.10/man
 export LM_LICENSE_FILE=$LM_LICENSE_FILE:$PGI/license.dat
 
-module load gcc/6.3.0-fasrc01
+#module load gcc/6.3.0-fasrc01
 
 git pull
 g++ q2_b.cpp -o q2_b_seq
