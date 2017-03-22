fea67d6de7105388ea8e99b9f196f1d9482c0922
Eric Dunipace <eifer@Cid.local>
2017-03-22 12:25:11 -0400

    cublas problems

--- CleanGPU/exercise_solutions/cuda/matmul_GPU_shmem/kernel.cu
+++ CleanGPU/exercise_solutions/cuda/matmul_GPU_shmem/kernel.cu
@@ -99,8 +99,8 @@ int main( int argc, char *argv[] )
 {
 
 /* get GPU device number and name */
-  int SIZE[3] = {pow(2,6), pow(2,10), pow(2,16)}
-for(size_t s = 0; s < 3; s++){
+  int SIZE[3] = {pow(2,6), pow(2,10), pow(2,16)};
+  for(size_t s = 0; s < 3; s++){
   const int size = SIZE[s];
   int dev;
   cudaDeviceProp deviceProp;
