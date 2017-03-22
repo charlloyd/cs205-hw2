b0010a88e8908ce10897840ea7d746d41c6a504a
charlloyd <char.lloyd@gmail.com>
2017-03-21 20:43:21 -0400

    Update helloacc.cpp

--- helloacc.cpp
+++ helloacc.cpp
@@ -2,14 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-// compile with "g++ helloacc.cpp -o helloacc"
-// compile with "pgc++ -acc helloacc.cpp -Minfo=accel"
-// execute with "./helloacc"
-
-using namespace std;
-
 int main () {
-   // for loop execution
 
 int b[8] = {1,2,3,4,5,6,7,8};
 int total=0;
@@ -17,10 +10,8 @@ int total=0;
 #pragma acc kernels
 #pragma acc data copyin(b), copyout(total)
    for(int a = 0; a < 8; a = a + 1 ) {
-      //cout << "Hello World! number: " << b[a] << endl;
       total += b[a];
    }
- 
    printf("result: %d \n",total);
    return 0;
 }
