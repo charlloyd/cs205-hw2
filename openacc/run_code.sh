072f4cd6748331fe6ef420a36e3783acb2c3fc8f
Eric Dunipace <eifer@Cid.local>
2017-03-23 16:27:20 -0400

    fixing sh file

--- openacc/run_code.sh
+++ openacc/run_code.sh
@@ -3,9 +3,7 @@ export PATH=$PGI/linux86-64/16.10/bin:$PATH
 export MANPATH=$MANPATH:$PGI/linux86-64/16.10/man
 export LM_LICENSE_FILE=$LM_LICENSE_FILE:$PGI/license.dat
 
-ORIGIN = "q3_test"
-
-git pull origin $ORIGIN
+git pull origin q3_test
 
 make all_seq
 make all_para
