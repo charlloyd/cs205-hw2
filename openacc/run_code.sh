a236a6009f570536925b0c13eeb430c52235fe6a
Eric Dunipace <eifer@Cid.local>
2017-03-23 16:25:54 -0400

    cublas problems

--- openacc/run_code.sh
+++ openacc/run_code.sh
@@ -3,7 +3,9 @@ export PATH=$PGI/linux86-64/16.10/bin:$PATH
 export MANPATH=$MANPATH:$PGI/linux86-64/16.10/man
 export LM_LICENSE_FILE=$LM_LICENSE_FILE:$PGI/license.dat
 
-git pull
+ORIGIN = "q3_test"
+
+git pull $ORIGIN
 
 make all_seq
 make all_para
