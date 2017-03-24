f227dbb3e5679cda8a093962dca151d3b345a8ad
charlloyd <char.lloyd@gmail.com>
2017-03-23 21:39:56 -0400

    Update q2b.sh

--- openacc/q2b.sh
+++ openacc/q2b.sh
@@ -7,7 +7,7 @@ export LM_LICENSE_FILE=$LM_LICENSE_FILE:$PGI/license.dat
 module load gcc/6.3.0-fasrc01
 
 git pull
-g++ q2_b.cpp -o q2_b_seq
-pgc++ -acc q2_b.cpp -Minfo=accel -o q2_b_para
-./q2_b_seq
-./q2_b_para
+g++ q2b.cpp -o q2b_seq
+pgc++ -acc q2b.cpp -Minfo=accel -o q2b_para
+./q2b_seq
+./q2b_para
