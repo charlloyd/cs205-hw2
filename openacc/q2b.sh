27c80ac6bf3276f4703865038a53e49d69b1ba58
charlloyd <char.lloyd@gmail.com>
2017-03-23 21:13:10 -0400

    Update q2b.sh

--- openacc/q2b.sh
+++ openacc/q2b.sh
@@ -3,6 +3,7 @@ export PATH=$PGI/linux86-64/16.10/bin:$PATH
 export MANPATH=$MANPATH:$PGI/linux86-64/16.10/man
 export LM_LICENSE_FILE=$LM_LICENSE_FILE:$PGI/license.dat
 
+#srun --mem-per-cpu=1000 -p holyseasgpu -n 1 --gres=gpu:1 --pty -t 0-01:00 /bin/bash
 module load gcc/6.3.0-fasrc01
 
 git pull
