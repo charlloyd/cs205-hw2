78843956821fe42d53bc1eec14c0cc1bcd79e1d7
Eric Dunipace <eifer@Cid.local>
2017-03-24 04:05:52 -0400

    fixing sh file

---
+++ SDK/src/matrixMul/run.sh
@@ -0,0 +1,7 @@
+export PGI=/n/seasfs03/IACS/cs205/pgi
+export PATH=$PGI/linux86-64/16.10/bin:$PATH
+export MANPATH=$MANPATH:$PGI/linux86-64/16.10/man
+export LM_LICENSE_FILE=$LM_LICENSE_FILE:$PGI/license.dat
+
+make
+
