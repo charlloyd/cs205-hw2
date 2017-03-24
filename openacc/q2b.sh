36b88db0ac67996c12e8aad0a2972fec8cfa05fe
charlloyd <char.lloyd@gmail.com>
2017-03-23 21:40:18 -0400

    Update q2b.sh

--- openacc/q2b.sh
+++ openacc/q2b.sh
@@ -4,7 +4,7 @@ export MANPATH=$MANPATH:$PGI/linux86-64/16.10/man
 export LM_LICENSE_FILE=$LM_LICENSE_FILE:$PGI/license.dat
 
 #srun --mem-per-cpu=1000 -p holyseasgpu -n 1 --gres=gpu:1 --pty -t 0-01:00 /bin/bash
-module load gcc/6.3.0-fasrc01
+#module load gcc/6.3.0-fasrc01
 
 git pull
 g++ q2b.cpp -o q2b_seq
