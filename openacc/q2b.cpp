9498571937c938b5f012b893ed8d33804fb2bea5
Eric Dunipace <eifer@Cid.local>
2017-03-24 05:11:53 -0400

    fixing sh file

--- openacc/q2b.cpp
+++ openacc/q2b.cpp
@@ -6,7 +6,7 @@ void matrix_multiply(int *a, int *b, int *c, int nrows_a, int ncols_a, int ncols
   #pragma acc enter data copyin(a[0:nrows_a * ncols_a],b[0:ncols_a * ncols_b])
   #pragma acc enter data create(c[0:nrows_a * ncols_b])
   #pragma acc data present(a[0:nrows_a * ncols_a], b[0:ncols_a * ncols_b], c[0:nrows_a * ncols_b])
-    {
+    
    #pragma acc kernels loop independent
    for(int i=0; i<nrows_a; i++)
    {
@@ -26,7 +26,6 @@ void matrix_multiply(int *a, int *b, int *c, int nrows_a, int ncols_a, int ncols
           c[i*ncols_b+j] = sum;
       }
    }
-    }
     #pragma acc exit data copyout(c[0:nrows_a * ncols_b])
     #pragma acc exit data delete(a[0:nrows_a * ncols_a], b[0:ncols_a * ncols_b])
 
@@ -50,7 +49,7 @@ int main(void)
      clock_t end = clock();
      int time_spent = (int)(end - begin) / CLOCKS_PER_SEC;
       
-printf("%d", 4);
+    printf("%d", 4);
      std::cout<<"problem size: "<<N*N<<" time:"<< time_spent << std::endl;
     printf("%d", 5);
   }
