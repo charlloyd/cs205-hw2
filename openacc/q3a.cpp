2ac58dbade7a14241aca4dc6625a5c9edaff56a8
Eric Dunipace <eifer@Cid.local>
2017-03-24 04:56:38 -0400

    fixing sh file

--- openacc/q3a.cpp
+++ openacc/q3a.cpp
@@ -5,6 +5,7 @@ void bfs(int *Adj, int n, int root)
    int *x      = (int *) malloc(sizeof(int) * n);
    int *last_x = (int *) malloc(sizeof(int) * n);
 
+   #pragma acc enter data copyin(Adj[0:n * n], x[0:n],last_x[0:n])
    for(int i=0;i < n; i++)
    {
      x[i] = 0; last_x[i] = 0;
@@ -15,11 +16,11 @@ void bfs(int *Adj, int n, int root)
    {
      copy_from_source_to_destination(x, last_x, n);
 
-
+       #pragma acc kernels loop independent
      for(int j=0; j < n; j++)
      {
         x[j] = 0;
-
+         #pragma acc kernels loop independent
         for(int k=0;k < n; k++)
         {
            x[j] = x[j] || (Adj[j*n+k] && last_x[k]);
@@ -33,6 +34,8 @@ void bfs(int *Adj, int n, int root)
      if (same(x,last_x,n)) break;
 
    }
+#pragma acc exit data copyout(x[0:n])
+#pragma acc exit data delete(Adj[0:n * n], last_x[0:n])
 }
 
 int main(void)
