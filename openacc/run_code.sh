a01044926f75e1f5d72c0e768756ff02a4c882a0
charlloyd <char.lloyd@gmail.com>
2017-03-24 13:30:20 -0400

    Update run_code.sh

--- openacc/run_code.sh
+++ openacc/run_code.sh
@@ -3,5 +3,6 @@ export PATH=$PGI/linux86-64/16.10/bin:$PATH
 export MANPATH=$MANPATH:$PGI/linux86-64/16.10/man
 export LM_LICENSE_FILE=$LM_LICENSE_FILE:$PGI/license.dat
 
-git pull
+module load gcc/6.3.0-fasrc01
 
+git pull
